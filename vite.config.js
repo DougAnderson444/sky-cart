db7bee338868317332e84d9f1c38034c3214f245
Doug A <douganderson444@gmail.com>
2022-11-22 13:41:04 -0400

    add lockfile

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import path from 'path'
 
+// import { fileURLToPath } from 'url';
+// const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
 const config = {
   plugins: [sveltekit()],
   resolve: {
